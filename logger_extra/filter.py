f0097c8fca910c968ecdd87d6a5a54e8bea26f9d
Kalle Jokinen <kalle.jokinen@leasedealit.fi>
2025-04-28 14:54:30 +0300

    fix: ignore conflicting context vars.

--- logger_extra/filter.py
+++ logger_extra/filter.py
@@ -1,7 +1,7 @@
 import logging
 
 from logger_extra.logger_context import get_logger_context
-from logger_extra.utils import json_serialize
+from logger_extra.utils import is_builtin_attr, json_serialize
 
 
 class LoggerContextFilter(logging.Filter):
@@ -12,6 +12,9 @@ class LoggerContextFilter(logging.Filter):
         logger_context = get_logger_context()
 
         for key, value in logger_context.items():
+            if is_builtin_attr(key):
+                continue
+
             record.__dict__[key] = json_serialize(value)
 
         return True
