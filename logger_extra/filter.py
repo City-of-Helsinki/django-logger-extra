b94ea1a89169004f5719a61125eb6732de225fe8
Kalle Jokinen <kalle.jokinen@gmail.com>
2025-04-28 15:38:28 +0300

    docs: Add description for LoggerContextFilter. (#5)
    Co-authored-by: Kalle Jokinen <kalle.jokinen@leasedealit.fi>

--- logger_extra/filter.py
+++ logger_extra/filter.py
@@ -5,6 +5,21 @@ from logger_extra.utils import is_builtin_attr, json_serialize
 
 
 class LoggerContextFilter(logging.Filter):
+    """
+    This filter merges user-provided extras with the active logger context variables.
+
+    Values are written directly into the LogRecord's __dict__, the same place native
+    extra fields are stored. This ensures that all formatters can access these context
+    fields seamlessly, treating them just like native `extra` fields.
+
+    For example, the following snippet stores both `greet` and `who` in the LogRecord:
+
+    ```python
+    with logger_context({"greet": "Hello"}) as ctx:
+        logger.info("Lorem Ipsum...", extra={"who": "World"})
+    ```
+    """
+
     def __init__(self, name: str = ""):
         super().__init__(name)
 
